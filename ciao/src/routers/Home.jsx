1f89af982f5c9bd8760b575395fd432095479be7
Jose Vitriago <80923963+Vic-iam@users.noreply.github.com>
2025-09-26 14:29:53 -0300

    Add DarkVeil animated background to Home page
    Introduced a new DarkVeil component using OGL for a generative animated background on the Home page. Updated Home.jsx to include the DarkVeil canvas and styled the layout for overlaying content. Added related CSS modules and updated dependencies to include gsap, @gsap/react, and ogl. Also adjusted Navbar background color and z-index for better layering.

--- ciao/src/routers/Home.jsx
+++ ciao/src/routers/Home.jsx
@@ -1,14 +1,30 @@
 'use client'
 import Navbar from "../components/Navbar"
+import "./Home.module.css"
+import DarkVeil from "../components/Darkveil"
 
-
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-        
-        <Navbar />
 
-         <h1>Hi</h1>
+            <Navbar />
+
+            <div className="app-container">
+                <DarkVeil
+                    hueShift={280}        // ~280° en el círculo de color = púrpura
+                    noiseIntensity={0.05} // un poco de ruido
+                    scanlineIntensity={0.1}
+                    scanlineFrequency={4}
+                    warpAmount={0.03}
+                    resolutionScale={1}
+                />
+
+                <div className="content">
+                    <h1>Ciao bella, tu estilo es la obra de arte que te define.✨</h1>
+                    <p>Sé valiente. Sé audaz. Sé simplemente hermosa.</p>
+                </div>
+            </div>
+
 
         </>
     )
